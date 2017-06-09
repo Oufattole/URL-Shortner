d955f6d7350b84dda75b5ac6f70af84ddb724897
medgar591 <medgar591@rsdmo.org>
2017-06-09 10:40:00 -0500

    Major Code Reorganization

--- shortner.pl
+++ shortner.pl
@@ -5,6 +5,7 @@ use Mojo::URL;
 my $f = "db.txt";	#text document used for link storage
 my $y;			#empty variable used for something later/file length??
 my $fh;			#file handler, integrated variable
+my %urls;
 
 ##Subroutines and Helpers##
 sub fhlen{ #Sets Y as the length of the file
@@ -17,46 +18,45 @@ sub fhlen{ #Sets Y as the length of the file
 	close $fh;
 }
 
+helper redirection => sub { #Helper that takes in a key and using that redirects the user to the appropriate site 
+	$c = shift;
+	my $small = $c->param('small');
+	my %urllist;
+	open($fh, "<", $f);
+	while(!eof $fh) {                              #while not at the end of the file
+		my $line = readline $fh;               #In a loop readline automatically goes line by line
+		if(defined $line){                     #Makes sure the line is not '' or '0'
+			my %words = split / /, $line;  #creates a hash with every word in the line
+			%urllist = (%urllist, %words); #appending the %words
+		}
+	}
+	close $fh;
+	my $link = $urs{$sma};   #Defines the long address
+  	chomp $s;             #Removes any whitespace - for safey
+	$c->redirect_to("$link");#The Redirect
+	
+
 #Main Page: uses index template
 get '/' => sub {
 	my  $c = shift;
 	$c->render(template => 'index');
 };
 
+#Redirection page which sends a person to their site, needs to have the key
+get '/a' => sub{
+	my $c = shift;
+	$c->redirection;
+};
+
 #Shortened Page: uses short template and gives the shortened link
-get '/newsurl' => sub {
+get '/long' => sub {
 	my $c = shift;
 	my $bigg = $c->param('big');
 	fhlen();  #Sets y as the length of the text file lines
 	open($fh, ">>", $f);
 	print $fh "\n", $y, " ", $bigg;
 	close $fh;
-	$c->render(template => 'short', burl => $y); #burl means SmallUrl - the placeholder number
-};
-
-#Input Key Page: uses 'notshort' template which has a textbox for the key
-get '/nshort' => sub{
-	my $c = shift;
-	$c->render(template => 'notshort'); 
-};
-
-#Key Page: takes in a key#after inputing small redirects to big
-get '/newburl' => sub {
-	my $c = shift;
-	my $sma = $c->param('small');
-	my %urs;
-	open($fh, "<", $f);
-	while(!eof $fh) {                             #while not at the end of the file
-		my $line = readline $fh;              #In a loop readline automatically goes line by line
-		if(defined $line){                    #Makes sure the line is not '' or '0'
-			my %words = split / /, $line; #creates a hash with every word in the line
-			%urs = (%urs, %words);        #appending the %words
-		}
-	}
-	close $fh;
-	my $s = $urs{$sma};   #Defines the long address
-  	chomp $s;             #Removes any whitespace - for safey
-	$c->redirect_to("$s");#The Redirect
+	$c->render(template => 'index', surl => $y); #surl means SmallUrl - the placeholder number
 };
 
 app->start;
@@ -71,22 +71,20 @@ __DATA__
 % layout 'default';
 % title 'Welcome';
 <h1>Enter the URL you would like shortened below!</h1>
-%= form_for newsurl => begin
+%= form_for long => begin
 %= text_field 'big'
 %= submit_button 
+%= if ( $y ne '' ) {
+	Please use the link <b>medgar.interns.kit.cm/a?small=$y</b> to access your site.
 %= end
 <h2> Or you can enter a shortened key below!</h2>
-%= form_for newburl => begin 
+%= form_for a => begin 
 %= text_field 'small'
 %= submit_button
 %= end
 
-@@ redir.html.ep
-<html><%= link_to 'hyper' => begin %>Les Goooo!!!<% end %>
-<html>
-
 @@ short.html.ep
-<html>The special key for your website is <b><%=$burl%></b>
+<html>The special key for your website is <b><%=$surl%></b>
 To go back click <%= link_to 'here' => '/'%>
 </html>
 
